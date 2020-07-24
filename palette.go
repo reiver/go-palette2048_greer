fb3c119edc370b01a862521f1c59a4b931943a68
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-23 20:38:27 -0700

    correction

--- palette.go
+++ palette.go
@@ -4,7 +4,7 @@ import (
 	"github.com/reiver/go-palette2048"
 )
 
-// Palette provides the NES palette.
+// Palette provides the Greer palette.
 var Palette [palette2048.ByteSize]uint8 = [palette2048.ByteSize]uint8{
 	0x29,0x09,0x31, 0xff,
 	0xee,0xff,0xf3, 0xff,
